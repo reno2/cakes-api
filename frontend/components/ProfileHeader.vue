0d3ac8a398bd64ef66d8cee99b17a1ddbd936511
an.zharskii <chedia@mail.ru>
2022-09-04 17:05:26 +0300

    WIP

---
+++ frontend/components/ProfileHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="profile-header">
+    <template v-if="$auth.loggedIn">
+      <a @click.prevent="logout" href="#">выйти</a>
+    </template>
+    <template v-else>
+      <a @click.prevent="login" href="#">войти</a>
+    </template>
+  </div>
+</template>
+<script>
+
+
+export default {
+  name: 'ProfileHeader',
+  props: {
+    menuItems: []
+  }
+}
+</script>
